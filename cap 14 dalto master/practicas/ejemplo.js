3b1754f3a334eb621d7cec13e383be2abc718f4f
MBraganzaa <josebraganza23@gmail.com>
2022-12-06 22:41:58 -0300

    commit(cap 14 de dalto master.)

--- cap 14 dalto master/practicas/ejemplo.js
+++ cap 14 dalto master/practicas/ejemplo.js
@@ -11,15 +11,37 @@
 //     console.log("modificado desde adentro")
 // }
 // console.log(objeto)
-// "use stritc";
+// "use strict";
 
-function saludar(){
-    this.nombre = "pedro";
-    this.saludar = function(){
-        console.log(`hola ${this.nombre}`);
-    } 
+// function saludar(){
+//     this.nombre = "pedro";
+//     this.saludar = function(){
+//         console.log(`hola ${this.nombre}`);
+//     } 
+// }
+
+// obj = new saludar();
+
+// obj.saludar();
+// const saludar = function(){return hey = "hola"};
+// const saludar = res => hey = res;
+
+// resultado = saludar("hola")
+
+// console.log(resultado)
+"use strict";
+
+const saludar = (mgs)=>{
+    let edad;
+    try{
+        if (mgs) edad = prompt(mgs)
+        else { edad = prompt("introduce tu edad")};
+        edad = parseInt(edad);
+        if(isNaN(edad))throw "introduce una edad valida"
+        if(edad > 18 ){alert("sos mayor de edad")}
+        else {alert("sos menor de edad")};
+    } catch(e){
+        saludar(e)
+    }
 }
-
-obj = new saludar();
-
-obj.saludar();
+saludar()
